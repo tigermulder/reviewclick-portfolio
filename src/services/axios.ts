bc938d011da06efcd2f5b66b121affe516d87f5c
Park HyunSung <hyunsung.park@jamonglab.com>
2024-11-05 15:34:18 +0900

    axios 인스턴스 수정

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -11,23 +11,35 @@ const axiosInstance: AxiosInstance = axios.create({
   withCredentials: true,
 })
 
-// 요청 인터셉터: 요청 시 파라미터에 토큰 포함 및 headers
+// 요청 인터셉터
 axiosInstance.interceptors.request.use(
   (config) => {
     const token = sessionStorage.getItem("authToken")
     if (token) {
-      // GET 요청일 경우 쿼리 파라미터에 추가
-      if (config.method === "get" && config.params) {
-        config.params.token = token
-      } else if (config.method === "post" || config.method === "put") {
-        if (config.data instanceof FormData) {
-          if (!config.data.has("token")) {
+      const hasTokenInParams = config.params && config.params.token
+      const hasTokenInData =
+        config.data &&
+        ((config.data instanceof FormData && config.data.has("token")) ||
+          (typeof config.data === "object" && config.data.token))
+
+      // 요청에 이미 토큰이 있는지 확인
+      const hasTokenInRequest = hasTokenInParams || hasTokenInData
+
+      if (!hasTokenInRequest) {
+        // GET 요청일 경우 쿼리 파라미터에 추가
+        if (config.method === "get") {
+          config.params = config.params || {}
+          config.params.token = token
+        }
+        // POST, PUT 등의 요청일 경우 요청 본문에 추가
+        else if (config.method === "post" || config.method === "put") {
+          if (config.data instanceof FormData) {
             config.data.append("token", token)
-          }
-        } else {
-          config.data = {
-            ...config.data,
-            token,
+          } else {
+            config.data = {
+              ...config.data,
+              token,
+            }
           }
         }
       }
@@ -35,10 +47,8 @@ axiosInstance.interceptors.request.use(
 
     // 데이터가 FormData일 경우 headers 수정
     if (config.data instanceof FormData) {
-      // 'Content-Type' 헤더를 삭제하여 브라우저가 자동으로 설정하게 함
       config.headers["Content-Type"] = "multipart/form-data"
     } else {
-      // JSON 데이터를 보낼 경우 'Content-Type'을 'application/json'으로 설정
       config.headers["Content-Type"] = "application/json"
     }
 
