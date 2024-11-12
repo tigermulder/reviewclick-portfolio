aa1011af46b73b5e7ba11a985ab57bf92d968d0c
Park HyunSung <hyunsung.park@jamonglab.com>
2024-11-12 16:59:06 +0900

    bottom탭바 수정

--- src/pages/views/CampaignDetailPage.tsx
+++ src/pages/views/CampaignDetailPage.tsx
@@ -126,7 +126,7 @@ const CampaignDetailPage = () => {
 
   //** 캠페인신청 모달 열기 [1-1] */
   const handleApply = () => {
-    const isLoggedIn = sessionStorage.getItem("email")
+    const isLoggedIn = localStorage.getItem("email")
     if (!isLoggedIn) {
       addToast("계정인증이 필요합니다.", "warning", 1000, "Join")
       navigate(RoutePath.Join, { replace: true })
