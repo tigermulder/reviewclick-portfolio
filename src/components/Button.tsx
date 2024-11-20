140a451cbef68809725ad1ae02426643c464efa7
Park HyunSung <hyunsung.park@jamonglab.com>
2024-11-20 15:14:35 +0900

    버튼수정

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -109,7 +109,7 @@ const StyledButton = styled.button.attrs<StyledButtonProps>((props) => ({
           font-weight: var(--font-bodyM-weight);
           line-height: var(--font-bodyM-line-height);
           letter-spacing: var(--font-bodyM-letter-spacing);
-          box-shadow: 0px 0px 12px rgba(246.44, 95.26, 102.39, 0.5);
+          box-shadow: 0px 0px 10px rgba(246.44, 95.26, 102.39, 0.3);
           border: 1px solid var(--prim-L60);
           color: var(--revu-color);
           display: flex;
