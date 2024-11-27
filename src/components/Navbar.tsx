8c378fd009757d7c6856da4387aacd34c99b8174
Park HyunSung <hyunsung.park@jamonglab.com>
2024-11-27 15:04:51 +0900

    필터드롭다운 말줄임표 속성 추가

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -207,9 +207,9 @@ const StyledIcon = styled.svg.attrs<{ $active: boolean; $tabName?: string }>(
     "aria-hidden": true,
   })
 )<{ $active: boolean; $tabName?: string }>`
-  width: ${({ $tabName }) => ($tabName === "user" ? "4.9rem" : "2rem")};
-  height: ${({ $tabName }) => ($tabName === "user" ? "4.9rem" : "2rem")};
-  margin-bottom: 0.7rem;
+  width: ${({ $tabName }) => ($tabName === "user" ? "4.9rem" : "1.7rem")};
+  height: ${({ $tabName }) => ($tabName === "user" ? "4.9rem" : "1.7rem")};
+  margin-bottom: 0.4rem;
   color: ${({ $active }) => ($active ? "var(--revu-color)" : "var(--silver)")};
 `
 
