7f74988af770472712d1a723b129bb7ecde225ba
Park HyunSung <hyunsung.park@jamonglab.com>
2024-12-09 17:52:42 +0900

    navbar css수정

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -240,8 +240,9 @@ const Circle = styled.span`
   position: absolute;
   top: 0;
   right: 0;
-  width: 2px;
-  height: 2px;
-  background: #000;
+  width: 0.4rem;
+  height: 0.4rem;
+  background-color: var(--prim-L400);
+  border: 0.25rem solid #fff;
   border-radius: 50%;
 `
