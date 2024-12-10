1a6e022f847c16f5d0086054934ec75fa6aa0a95
Park HyunSung <hyunsung.park@jamonglab.com>
2024-12-10 10:52:48 +0900

    서비스 이용가이드 page 수정

--- src/components/SinglePageHeader.tsx
+++ src/components/SinglePageHeader.tsx
@@ -29,7 +29,7 @@ const HeaderContainer = styled.div`
   left: 0;
   z-index: 3;
   width: 100%;
-  padding: 1.9rem 1.5rem;
+  padding: 1.5rem;
   background: var(--white);
   box-shadow: 0 1px 2px rgba(0, 0, 0, 0.03);
 `
