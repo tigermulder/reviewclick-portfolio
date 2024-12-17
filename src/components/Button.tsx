7f5151e709e696e5be78212a39bb24d3bb10c183
Park HyunSung <hyunsung.park@jamonglab.com>
2024-12-17 11:44:02 +0900

    스타일가이드 수정

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -31,7 +31,7 @@ const Dot = styled.div`
   height: 0.55rem;
   background-color: var(--L100);
   border-radius: 0.15rem;
-  transform-origin: center 0.85rem;
+  transform-origin: center 0.86rem;
   animation: ${fade} 1s linear infinite;
 
   &:nth-child(1) {
