b789fd818a70c1b5060900dc155fbc18cdcdb17d
weekendtiger <97021279+tigermulder@users.noreply.github.com>
2025-07-03 15:27:24 +0900

    루트 경로 라우팅 수정으로 메인페이지 정상 로드 되도록 개선

--- .history/src/pages/AppRoute_20241225230226.tsx
+++ .history/src/pages/AppRoute_20241225230226.tsx
@@ -54,7 +54,6 @@ export const AppRoute = () => {
           {/* Introduce 페이지 */}
           {/* <Route path={RoutePath.Introduce} element={<RevuIntroducePage />} /> */}
           {/* main 페이지 */}
-          <Route index element={<MainPage />} />
           <Route path={RoutePath.Home} element={<MainPage />} />
           {/* 캠페인상세 페이지 */}
           <Route
